d0e636dcbe23d709a3b26d91ff3291e919ec2d37
nobeeakon <nobeeakon@gmail.com>
2023-05-02 19:12:28 -0600

    deploy (#8)

--- start.sh
+++ start.sh
@@ -6,5 +6,21 @@
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
 set -ex
+
+# using swap according to https://github.com/remix-run/indie-stack/issues/200#issuecomment-1479622320
+# Setup 512MB of space for swap and set permissions and turn on swapmode
+fallocate -l 512M /swapfile
+chmod 0600 /swapfile
+mkswap /swapfile
+echo 10 > /proc/sys/vm/swappiness
+swapon /swapfile
+
+# Run migrations
 npx prisma migrate deploy
+
+# Turn off swap mode and remove swap directory
+swapoff /swapfile
+rm /swapfile
+
+# Finally start the app
 npm run start
